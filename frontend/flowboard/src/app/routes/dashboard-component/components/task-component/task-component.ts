f16e06e2ab358297d2b1c63d0a63cc37ff074582
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2025-12-21 21:11:10 -0300

    create checkbox for done WIP

--- frontend/flowboard/src/app/routes/dashboard-component/components/task-component/task-component.ts
+++ frontend/flowboard/src/app/routes/dashboard-component/components/task-component/task-component.ts
@@ -1,19 +1,25 @@
-import { Component, EventEmitter, Output, input } from '@angular/core';
+import { Component, EventEmitter, OnChanges, Output, SimpleChange, input } from '@angular/core';
 import { Task } from '../../../../models';
 import { DashboardService } from '../../dashboard-service';
 import { EditButtonComponent } from '../../../../components/edit-button-component/edit-button-component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-component',
-  imports: [EditButtonComponent],
+  imports: [EditButtonComponent, FormsModule],
   templateUrl: './task-component.html',
   styleUrl: './task-component.css',
 })
-export class TaskComponent {
+export class TaskComponent implements OnChanges {
   @Output() onDelete = new EventEmitter();
   task = input<Task>();
+  done = false;
 
   constructor(private service: DashboardService) {}
+
+  ngOnChanges(changes: { [propName: string]: SimpleChange<Task> }): void {
+    this.done = changes['task'].currentValue.done;
+  }
 
   delete() {
     this.service.deleteTask(this.task()!.id).subscribe(() => this.onDelete.emit());
