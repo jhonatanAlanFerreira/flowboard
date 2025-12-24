2afc09faf8d9d77bb2a8aa53971cf0468be8d2a1
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2025-12-24 19:21:32 -0300

    add workspace delete confirmation

--- frontend/flowboard/src/app/components/dialog-component/dialog-component.ts
+++ frontend/flowboard/src/app/components/dialog-component/dialog-component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Output, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Button } from 'primeng/button';
 import { Dialog } from 'primeng/dialog';
 
 @Component({
   selector: 'app-dialog-component',
-  imports: [Dialog, Button],
+  imports: [Dialog, Button, ReactiveFormsModule],
   templateUrl: './dialog-component.html',
   styleUrl: './dialog-component.css',
 })
@@ -12,6 +13,23 @@ export class DialogComponent {
   @Output() onConfirm = new EventEmitter();
   @Output() onCancel = new EventEmitter();
 
+  confirmationControl = new FormControl<string | null>(null);
+
   header = input.required<string>();
   visible = input(false);
+  inputValueConfirmation = input<string>();
+
+  confirm() {}
+
+  onHide() {
+    this.confirmationControl.reset();
+  }
+
+  get isDisabled() {
+    return (
+      !!this.inputValueConfirmation() &&
+      this.inputValueConfirmation()?.toLowerCase() !=
+        this.confirmationControl.value?.toLowerCase()
+    );
+  }
 }
