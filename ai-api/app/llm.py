7994912f5b6a6833718c1c13b093ccb8d1869e24
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2026-03-27 19:50:31 -0300

    load llm models in worker only

--- ai-api/app/llm.py
+++ ai-api/app/llm.py
@@ -5,18 +5,29 @@ _llm = None  # singleton instance
 
 
 def get_llm():
+    """
+    Returns a singleton Llama instance.
+    Loads the model only if LOAD_LLM=1.
+    """
+
     global _llm
 
     if _llm is None:
-        _llm = Llama(
-            model_path=os.getenv(
-                "MODEL_PATH"
-            ),
-            n_ctx=1024,
-            n_threads=8,
-            n_batch=512,
-            use_mmap=True,
-            use_mlock=True
-        )
+        if os.getenv("LOAD_LLM", "0") == "1":
+            model_path = os.getenv("MODEL_PATH")
+            if not model_path:
+                raise ValueError("MODEL_PATH environment variable is not set")
+
+            _llm = Llama(
+                model_path=model_path,
+                n_ctx=1024,
+                n_threads=8,
+                n_batch=512,
+                use_mmap=True,
+                use_mlock=True,
+            )
+        else:
+            # API container or other services won’t load the model
+            _llm = None
 
     return _llm
